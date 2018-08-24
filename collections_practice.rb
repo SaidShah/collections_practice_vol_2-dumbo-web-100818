7cd1e8ab166778f6f9b0c51313e9bc66a62ce1cf
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-24 02:27:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -75,7 +75,7 @@ def organize_schools(given_array)
   given_array.values.each { |location| locations[location.values[0]] = [] }
   given_array.each { |school, location| locations[location.values[0]] << school }
   locations
-
+  binding.pry
 end
 
 
